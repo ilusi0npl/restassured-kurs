04ddfa8e09fc077bd85eb11658e8fe4bb2fc4122
Mateusz Ciołek <mateuszciolek@gmail.com>
2020-04-19 08:20:33 +0200

    Fixed class formatting

--- src/main/java/pl/javastart/main/pojo/Category.java
+++ src/main/java/pl/javastart/main/pojo/Category.java
@@ -3,23 +3,23 @@ package pl.javastart.main.pojo;
 
 public class Category {
 
-private Integer id;
-private String name;
+    private Integer id;
+    private String name;
+    
+    public Integer getId() {
+        return id;
+    }
 
-public Integer getId() {
-return id;
-}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-public void setId(Integer id) {
-this.id = id;
-}
+    public String getName() {
+        return name;
+    }
 
-public String getName() {
-return name;
-}
-
-public void setName(String name) {
-this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
--- src/main/java/pl/javastart/main/pojo/Pet.java
+++ src/main/java/pl/javastart/main/pojo/Pet.java
@@ -4,59 +4,59 @@ import java.util.List;
 
 public class Pet {
 
-private Integer id;
-private Category category;
-private String name;
-private List<String> photoUrls = null;
-private List<Tag> tags = null;
-private String status;
+    private Integer id;
+    private Category category;
+    private String name;
+    private List<String> photoUrls = null;
+    private List<Tag> tags = null;
+    private String status;
 
-public Integer getId() {
-return id;
-}
+    public Integer getId() {
+        return id;
+    }
 
-public void setId(Integer id) {
-this.id = id;
-}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-public Category getCategory() {
-return category;
-}
+    public Category getCategory() {
+        return category;
+    }
 
-public void setCategory(Category category) {
-this.category = category;
-}
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 
-public String getName() {
-return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-public List<String> getPhotoUrls() {
-return photoUrls;
-}
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
 
-public void setPhotoUrls(List<String> photoUrls) {
-this.photoUrls = photoUrls;
-}
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
 
-public List<Tag> getTags() {
-return tags;
-}
+    public List<Tag> getTags() {
+        return tags;
+    }
 
-public void setTags(List<Tag> tags) {
-this.tags = tags;
-}
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 
-public String getStatus() {
-return status;
-}
+    public String getStatus() {
+        return status;
+    }
 
-public void setStatus(String status) {
-this.status = status;
-}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
 }
