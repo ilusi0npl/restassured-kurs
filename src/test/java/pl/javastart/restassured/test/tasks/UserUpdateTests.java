adef9463d4f1bc675345273bb9dcb8b2288afd73
Mateusz Ciołek <mateuszciolek@gmail.com>
2020-04-19 08:51:16 +0200

    Created test UserUpdateTests

---
+++ src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTests.java
@@ -0,0 +1,45 @@
+package pl.javastart.restassured.test.tasks;
+
+import org.testng.annotations.Test;
+import pl.javastart.main.pojo.user.User;
+
+import static io.restassured.RestAssured.given;
+
+public class UserUpdateTests {
+
+    @Test
+    public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
+        User user = new User();
+        user.setId(445);
+        user.setUsername("firstuser");
+        user.setFirstName("Krzysztof");
+        user.setLastName("Kowalski");
+        user.setEmail("krzysztof@test.com");
+        user.setPassword("password");
+        user.setPhone("+123456789");
+        user.setUserStatus(123);
+
+        given().log().all()
+                .contentType("application/json")
+                .body(user)
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .then().log().all().statusCode(200);
+
+        user.setFirstName("Adam");
+        user.setLastName("Malinowski");
+
+        given().log().all()
+                .contentType("application/json")
+                .pathParam("username", user.getUsername())
+                .body(user)
+                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .then().log().all().statusCode(200);
+
+        given().log().all()
+                .contentType("application/json")
+                .pathParam("username", user.getUsername())
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .then().log().all().statusCode(200);
+    }
+
+}
