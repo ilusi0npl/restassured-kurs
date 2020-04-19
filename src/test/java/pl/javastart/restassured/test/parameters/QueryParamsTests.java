101250714eb5a843931147e820ddc276b3db8085
Mateusz Ciołek <mateuszciolek@gmail.com>
2020-04-19 16:00:26 +0200

    Created QueryParamsTests

---
+++ src/test/java/pl/javastart/restassured/test/parameters/QueryParamsTests.java
@@ -0,0 +1,45 @@
+package pl.javastart.restassured.test.parameters;
+
+import org.testng.annotations.Test;
+import pl.javastart.main.pojo.Category;
+import pl.javastart.main.pojo.Pet;
+import pl.javastart.main.pojo.Tag;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertTrue;
+
+public class QueryParamsTests {
+
+    @Test
+    public void givenExistingPetWithStatusSoldWhenGetPetWithSoldStatusThenPetWithStatusIsReturnedTest() {
+        Category category = new Category();
+        category.setId(777);
+        category.setName("dogs");
+
+        Tag tag = new Tag();
+        tag.setId(1);
+        tag.setName("dogs-category");
+
+        Pet pet = new Pet();
+        pet.setId(123);
+        pet.setCategory(category);
+        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
+        pet.setTags(Collections.singletonList(tag));
+        pet.setStatus("sold");
+
+        given().log().all().body(pet).contentType("application/json")
+                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/pet")
+                .then().log().all().statusCode(200);
+
+        Pet[] pets = given().log().all().body(pet).contentType("application/json")
+                .queryParam("status", "sold")
+                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/pet/findByStatus")
+                .then().log().all().statusCode(200).extract().as(Pet[].class);
+
+        assertTrue(Arrays.asList(pets).size() > 0, "List of pets");
+    }
+
+}
