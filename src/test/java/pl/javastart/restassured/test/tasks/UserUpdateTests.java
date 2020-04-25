a239703a5f8b590a23a9f28b110bb06d6a413744
Mateusz Ciołek <mciolek@cloudentity.com>
2020-04-25 08:44:13 +0200

    Added TestBase and extended tests

--- src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserUpdateTests.java
@@ -5,7 +5,7 @@ import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
 
-public class UserUpdateTests {
+public class UserUpdateTests extends TestBase {
 
     @Test
     public void givenCorrectUserDataWhenFirstNameLastNameAreUpdatedThenUserDataIsUpdatedTest() {
@@ -22,7 +22,7 @@ public class UserUpdateTests {
         given().log().all()
                 .contentType("application/json")
                 .body(user)
-                .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
+                .when().post("user")
                 .then().log().all().statusCode(200);
 
         user.setFirstName("Adam");
@@ -32,13 +32,13 @@ public class UserUpdateTests {
                 .contentType("application/json")
                 .pathParam("username", user.getUsername())
                 .body(user)
-                .when().put("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .when().put("user/{username}")
                 .then().log().all().statusCode(200);
 
         given().log().all()
                 .contentType("application/json")
                 .pathParam("username", user.getUsername())
-                .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
+                .when().get("user/{username}")
                 .then().log().all().statusCode(200);
     }
 
