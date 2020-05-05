6419a1fb0cbca0ab0f03e7f8d4b40453a4130632
Mateusz Ciołek <mciolek@cloudentity.com>
2020-05-05 18:51:33 +0200

    Refactored UserCreationTests.java to use hamcrest assertions

--- src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
+++ src/test/java/pl/javastart/restassured/test/tasks/UserCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import pl.javastart.main.pojo.user.User;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UserCreationTests extends TestBase {
 
@@ -22,12 +23,25 @@ public class UserCreationTests extends TestBase {
         given().contentType("application/json")
                 .body(user)
                 .when().post("user")
-                .then().statusCode(200);
+                .then()
+                .assertThat().body("code", equalTo(200))
+                .assertThat().body("type", equalTo("unknown"))
+                .assertThat().body("message", equalTo("445"))
+                .assertThat().statusCode(200);
 
         given().contentType("application/json")
                 .pathParam("username", user.getUsername())
                 .when().get("user/{username}")
-                .then().statusCode(200);
+                .then()
+                .assertThat().body("code", equalTo(445))
+                .assertThat().body("username", equalTo("firstuser"))
+                .assertThat().body("firstName", equalTo("Krzysztof"))
+                .assertThat().body("lastName", equalTo("Kowalski"))
+                .assertThat().body("email", equalTo("krzysztof@test.com"))
+                .assertThat().body("password", equalTo("password"))
+                .assertThat().body("phone", equalTo("+123456789"))
+                .assertThat().body("userStatus", equalTo(123))
+                .assertThat().statusCode(200);
     }
 
 }
