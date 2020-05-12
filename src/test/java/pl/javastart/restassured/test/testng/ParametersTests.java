7beac87ee44a1434f83c926d072e48ab5d44fbdf
Mateusz Ciołek <mciolek@cloudentity.com>
2020-05-12 20:07:43 +0200

    Created initial ParametersTests.java

---
+++ src/test/java/pl/javastart/restassured/test/testng/ParametersTests.java
@@ -0,0 +1,32 @@
+package pl.javastart.restassured.test.testng;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ParametersTests {
+
+    @BeforeMethod
+    public void beforeMethod() {
+        RestAssured.baseURI = "https://swaggerpetstore.przyklady.javastart.pl";
+        RestAssured.basePath = "v2";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+    @Parameters({"petId", "expectedMessage"})
+    @Test
+    public void givenNonExistingPetIdWhenGetPetThenPetNotFoundTest(@Optional("0") String petId,
+                                                                   @Optional("Pet not found") String message) {
+        given()
+                .when().get("pet/{param}", petId)
+                .then().statusCode(404).assertThat().body("message", equalTo(message));
+    }
+
+}
